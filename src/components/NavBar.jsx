2f9b3d359962f5206c7dd790e81adb6e16de8856
Lukkas98 <lucaspalma988@gmail.com>
2023-03-29 17:41:40 -0300

    styles

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,11 +3,11 @@ import { NavLink } from "react-router-dom"
 const NavBar= ()=>{
 
     return(
-        <>  
-            <NavLink to="/home" >Home</NavLink>
-            <NavLink to="/profile/:id" >Perfil</NavLink>
-            <NavLink to="/" >Cerrar sesión</NavLink>
-        </>
+        <div className="bg-green-600 bg-opacity-40 w-1/4 flex flex-col items-center justify-evenly h-full" >  
+            <NavLink to="/home" className="rounded-md shadow-xs bg-blue-800 py-1 px-2" >Home</NavLink>
+            <NavLink to="/profile/:id" className="rounded-md shadow-xs bg-blue-800 py-1 px-2" >Perfil</NavLink>
+            <NavLink to="/" className="rounded-md shadow-xs bg-blue-800 py-1 px-2" >Cerrar sesión</NavLink>
+        </div>
     )
 }
 
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,9 +1,9 @@
 const SideBar = ()=>{
 
     return(
-        <>
+        <div className="bg-green-600 bg-opacity-40 w-1/4 flex flex-col items-center justify-evenly h-full">
             <p>Barra Lateral</p>
-        </>
+        </div>
     )
 }
 //aca va la search bar y los favoritos guardados
