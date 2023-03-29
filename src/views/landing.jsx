09576d5a39ed8bf2655651f396e0d4dffa3b3f5b
Ailín <ailingalanterosso@gmail.com>
2023-03-29 02:24:44 -0300

    new views added

--- src/views/landing.jsx
+++ src/views/landing.jsx
@@ -6,6 +6,7 @@ const Landing = ()=>{
         <>
             <p>Bienvenido a ProgramaRed</p>
             <NavLink to="/home">Entrar</NavLink>
+            <NavLink to="/signUp">Registrarse</NavLink>
         </>
     )
 }
