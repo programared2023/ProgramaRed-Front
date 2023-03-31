31abf2f2ef97e1dac203128e0edaaf33913f1f9a
Lukkas98 <lucaspalma988@gmail.com>
2023-03-31 10:00:30 -0300

    fix de estilos y hardcodeo

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,17 +1,16 @@
 import { NavLink } from "react-router-dom";
 
-const Post = ( {post} )=>{
+const Post = ( {post, user} )=>{
 
     return(
-        <div className=" flex flex-col items-center justify-center gap-2">
 
             <div className="bg-green-100 rounded-lg p-4 mt-4 shadow-shadowBlack">
                 <div className="flex items-center mb-2">
                     <div className="bg-green-300 w-12 h-12 rounded-full mr-3">
-                        <img src={post.author.image} alt="" className="w-full"/>
+                        <img src={user.image ? user.image : "https://source.unsplash.com/random/500x500"} alt={user.username} className="w-full"/>
                     </div>
                     <div className="flex-1">
-                    <NavLink to={`/profile/${post.author.name}`} className="text-green-700 font-medium text-sm">{post.author.name}</NavLink>
+                    <NavLink to={`/profile/${user.id}`} className="text-green-700 font-medium text-sm">{user.username}</NavLink>
                     <p className="text-green-500 text-xs">hace 2 horas</p>
                     </div>
                 </div>
@@ -20,7 +19,6 @@ const Post = ( {post} )=>{
                     {post.description}
                 </NavLink>
             </div>
-        </div>
     )
 }
 
--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,76 +1,53 @@
 import Post from "./Post";
 
-const posts = 
-    [
-        {
-          "author": {
-            "id": 1,
-            "name": "John Doe",
-            "email": "johndoe@example.com",
-            "image": "https://example.com/images/johndoe.jpg"
-          },
-          "id": 1,
-          "title": "Título del primer objeto",
-          "description": "Este objeto es un artículo que ha sido cuidadosamente diseñado para satisfacer tus necesidades. Es un producto de alta calidad que está hecho con materiales duraderos y resistentes, lo que significa que durará mucho tiempo. Este objeto también es muy versátil y se puede utilizar de muchas maneras diferentes, lo que lo convierte en una opción ideal para cualquier persona que busque un producto que pueda adaptarse a sus necesidades. Además, este objeto es muy fácil de usar y mantener, lo que lo hace perfecto para personas ocupadas o para aquellos que buscan un producto de bajo mantenimiento. En resumen, este objeto es una excelente opción para cualquiera que busque un producto de alta calidad y versátil que pueda satisfacer una amplia gama de necesidades."
-        },
-        {
-          "author": {
-            "id": 2,
-            "name": "Jane Doe",
-            "email": "janedoe@example.com",
-            "image": "https://example.com/images/janedoe.jpg"
-          },
-          "id": 2,
-          "title": "Título del segundo objeto",
-          "description": "Morbi mollis tellus ac sapien. Nunc id risus nec urna dignissim dignissim. Vivamus euismod nisi vel augue. Curabitur vestibulum aliquam leo. Praesent egestas neque eu enim. In hac habitasse platea dictumst."
-        },
-        {
-          "author": {
-            "id": 3,
-            "name": "Bob Smith",
-            "email": "bobsmith@example.com",
-            "image": "https://example.com/images/bobsmith.jpg"
-          },
-          "id": 3,
-          "title": "Título del tercer objeto",
-          "description": "Etiam sapien elit, consequat eget, tristique non, venenatis quis, ante. Nullam vestibulum elementum urna. Phasellus rhoncus. Praesent pharetra, magna non tempor tincidunt, felis tortor ultricies tellus, ac venenatis arcu wisi vel nisl. Vestibulum suscipit nulla quis orci."
-        },
-        {
-          "author": {
-            "id": 4,
-            "name": "Alice Johnson",
-            "email": "alicejohnson@example.com",
-            "image": "https://example.com/images/alicejohnson.jpg"
-          },
-          "id": 4,
-          "title": "Título del cuarto objeto",
-          "description": "Vestibulum volutpat pretium libero. Cras id dui. Aenean ut eros et nisl sagittis vestibulum. Nullam nulla eros, ultricies sit amet, nonummy id, imperdiet feugiat, pede. Sed lectus. Donec mollis hendrerit risus."
-        },
-        {
-          "author": {
-            "id": 5,
-            "name": "Mark Lee",
-            "email": "marklee@example.com",
-            "image": "https://example.com/images/marklee.jpg"
-          },
-          "id": 5,
-          "title": "Título del quinto objeto",
-          "description": "Phasellus nec sem in justo pellentesque facilisis. Etiam imperdiet imperdiet orci. Nunc nec neque. Phasellus leo dolor, tempus non, auctor et, hendrerit quis, nisi. Curabitur ligula sapien, tincidunt"
-        }
+//mas hardcodeo, acá tendria que haber estado local, useEffect y por supuesto estados globales
+const posts = [
+  {
+    id: 1,
+    title: "Alto estres",
+    description: "Este objeto es un artículo que ha sido cuidadosamente diseñado para satisfacer tus necesidades. Es un producto de alta calidad que está hecho con materiales duraderos y resistentes, lo que significa que durará mucho tiempo. Este objeto también es muy versátil y se puede utilizar de muchas maneras diferentes, lo que lo convierte en una opción ideal para cualquier persona que busque un producto que pueda adaptarse a sus necesidades. Además, este objeto es muy fácil de usar y mantener, lo que lo hace perfecto para personas ocupadas o para aquellos que buscan un producto de bajo mantenimiento. En resumen, este objeto es una excelente opción para cualquiera que busque un producto de alta calidad y versátil que pueda satisfacer una amplia gama de necesidades.",
+    file: "javascript.png",
+    createdAt: "2023-03-31T12:17:17.736Z",
+    updatedAt: "2023-03-31T12:17:17.736Z",
+    UserId: 1
+  },
+  {
+    id: 1,
+    title: "Segundo post",
+    description: "Una descripcion random de un post random...",
+    file: "javascript.png",
+    createdAt: "2023-03-31T12:17:17.736Z",
+    updatedAt: "2023-03-31T12:17:17.736Z",
+    UserId: 1
+  },
+  {
+    id: 2,
+    title: "Tercer Posteo",
+    description: "Este objeto es un artículo que ha sido cuidadosamente diseñado para satisfacer tus necesidades. Es un producto de alta calidad que está hecho con materiales duraderos y resistentes, lo que significa que durará mucho tiempo. Este objeto también es muy versátil y se puede utilizar de muchas maneras diferentes, lo que lo convierte en una opción ideal para cualquier persona que busque un producto que pueda adaptarse a sus necesidades. Además, este objeto es muy fácil de usar y mantener, lo que lo hace perfecto para personas ocupadas o para aquellos que buscan un producto de bajo mantenimiento. En resumen, este objeto es una excelente opción para cualquiera que busque un producto de alta calidad y versátil que pueda satisfacer una amplia gama de necesidades.",
+    file: "javascript.png",
+    createdAt: "2023-03-31T12:17:17.736Z",
+    updatedAt: "2023-03-31T12:17:17.736Z",
+    UserId: 2
+  }
 ]
 
+const user = {
+  id: 1,
+  username: "maria",
+  password: "clave123",
+  email: "maria@example.com",
+  birthdate: "1989-06-15",
+}
 
 const Posts = ()=>{
 
     return(
         <div className="flex flex-wrap justify-center items-center h-screen p-4">
+          <div className=" flex flex-col gap-2">
             {
-                posts.map((post, i) => {
-                    return(
-                        <Post post={post} key={i} />
-                    )
-                })
+              posts.map((post, i) => <Post user={user} post={post} key={i} />)
             }
+          </div>
         </div>
     )
 }
