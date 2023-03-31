5dd7fb81dd3b2520d9b0a43dc3aa744b499fe9c6
Lukkas98 <lucaspalma988@gmail.com>
2023-03-31 12:05:45 -0300

    styles fix home

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -12,7 +12,7 @@ const posts = [
     UserId: 1
   },
   {
-    id: 1,
+    id: 2,
     title: "Segundo post",
     description: "Una descripcion random de un post random...",
     file: "javascript.png",
@@ -21,13 +21,31 @@ const posts = [
     UserId: 1
   },
   {
-    id: 2,
+    id: 3,
     title: "Tercer Posteo",
     description: "Este objeto es un artículo que ha sido cuidadosamente diseñado para satisfacer tus necesidades. Es un producto de alta calidad que está hecho con materiales duraderos y resistentes, lo que significa que durará mucho tiempo. Este objeto también es muy versátil y se puede utilizar de muchas maneras diferentes, lo que lo convierte en una opción ideal para cualquier persona que busque un producto que pueda adaptarse a sus necesidades. Además, este objeto es muy fácil de usar y mantener, lo que lo hace perfecto para personas ocupadas o para aquellos que buscan un producto de bajo mantenimiento. En resumen, este objeto es una excelente opción para cualquiera que busque un producto de alta calidad y versátil que pueda satisfacer una amplia gama de necesidades.",
     file: "javascript.png",
     createdAt: "2023-03-31T12:17:17.736Z",
     updatedAt: "2023-03-31T12:17:17.736Z",
     UserId: 2
+  },
+  {
+    id: 4,
+    title: "Cuarto Posteo",
+    description: "Este objeto es un artículo que ha sido cuidadosamente diseñado para satisfacer tus necesidades. Es un producto de alta calidad que está hecho con materiales duraderos y resistentes, lo que significa que durará mucho tiempo. Este objeto también es muy versátil y se puede utilizar de muchas maneras diferentes, lo que lo convierte en una opción ideal para cualquier persona que busque un producto que pueda adaptarse a sus necesidades. Además, este objeto es muy fácil de usar y mantener, lo que lo hace perfecto para personas ocupadas o para aquellos que buscan un producto de bajo mantenimiento. En resumen, este objeto es una excelente opción para cualquiera que busque un producto de alta calidad y versátil que pueda satisfacer una amplia gama de necesidades.",
+    file: "javascript.png",
+    createdAt: "2023-03-31T12:17:17.736Z",
+    updatedAt: "2023-03-31T12:17:17.736Z",
+    UserId: 2
+  },
+  {
+    id: 5,
+    title: "Quinto Posteo",
+    description: "Este objeto es un artículo que ha sido cuidadosamente diseñado para satisfacer tus necesidades. Es un producto de alta calidad que está hecho con materiales duraderos y resistentes, lo que significa que durará mucho tiempo. Este objeto también es muy versátil y se puede utilizar de muchas maneras diferentes, lo que lo convierte en una opción ideal para cualquier persona que busque un producto que pueda adaptarse a sus necesidades. Además, este objeto es muy fácil de usar y mantener, lo que lo hace perfecto para personas ocupadas o para aquellos que buscan un producto de bajo mantenimiento. En resumen, este objeto es una excelente opción para cualquiera que busque un producto de alta calidad y versátil que pueda satisfacer una amplia gama de necesidades.",
+    file: "javascript.png",
+    createdAt: "2023-03-31T12:17:17.736Z",
+    updatedAt: "2023-03-31T12:17:17.736Z",
+    UserId: 1
   }
 ]
 
@@ -39,11 +57,13 @@ const user = {
   birthdate: "1989-06-15",
 }
 
+console.log(posts);
+
 const Posts = ()=>{
 
     return(
-        <div className="flex flex-wrap justify-center items-center h-screen p-4">
-          <div className=" flex flex-col gap-2">
+        <div className="flex flex-wrap justify-center items-center h-3/4 p-4">
+          <div className=" flex flex-col gap-2 overflow-y-auto h-full">
             {
               posts.map((post, i) => <Post user={user} post={post} key={i} />)
             }
