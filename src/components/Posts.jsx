904e73357e3874bd4c580b05108b1ceeada334d5
Lukkas98 <lucaspalma988@gmail.com>
2023-03-31 19:48:42 -0300

    fix posts

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -3,69 +3,10 @@ import Post from "./Post";
 import { useEffect } from "react";
 import { getAllPosts } from "../redux/actions";
 
-//mas hardcodeo, acá tendria que haber estado local, useEffect y por supuesto estados globales
-/* const posts = [
-  {
-    id: 1,
-    title: "Alto estres",
-    description: "Este objeto es un artículo que ha sido cuidadosamente diseñado para satisfacer tus necesidades. Es un producto de alta calidad que está hecho con materiales duraderos y resistentes, lo que significa que durará mucho tiempo. Este objeto también es muy versátil y se puede utilizar de muchas maneras diferentes, lo que lo convierte en una opción ideal para cualquier persona que busque un producto que pueda adaptarse a sus necesidades. Además, este objeto es muy fácil de usar y mantener, lo que lo hace perfecto para personas ocupadas o para aquellos que buscan un producto de bajo mantenimiento. En resumen, este objeto es una excelente opción para cualquiera que busque un producto de alta calidad y versátil que pueda satisfacer una amplia gama de necesidades.",
-    file: "javascript.png",
-    createdAt: "2023-03-31T12:17:17.736Z",
-    updatedAt: "2023-03-31T12:17:17.736Z",
-    UserId: 1
-  },
-  {
-    id: 2,
-    title: "Segundo post",
-    description: "Una descripcion random de un post random...",
-    file: "javascript.png",
-    createdAt: "2023-03-31T12:17:17.736Z",
-    updatedAt: "2023-03-31T12:17:17.736Z",
-    UserId: 1
-  },
-  {
-    id: 3,
-    title: "Tercer Posteo",
-    description: "Este objeto es un artículo que ha sido cuidadosamente diseñado para satisfacer tus necesidades. Es un producto de alta calidad que está hecho con materiales duraderos y resistentes, lo que significa que durará mucho tiempo. Este objeto también es muy versátil y se puede utilizar de muchas maneras diferentes, lo que lo convierte en una opción ideal para cualquier persona que busque un producto que pueda adaptarse a sus necesidades. Además, este objeto es muy fácil de usar y mantener, lo que lo hace perfecto para personas ocupadas o para aquellos que buscan un producto de bajo mantenimiento. En resumen, este objeto es una excelente opción para cualquiera que busque un producto de alta calidad y versátil que pueda satisfacer una amplia gama de necesidades.",
-    file: "javascript.png",
-    createdAt: "2023-03-31T12:17:17.736Z",
-    updatedAt: "2023-03-31T12:17:17.736Z",
-    UserId: 2
-  },
-  {
-    id: 4,
-    title: "Cuarto Posteo",
-    description: "Este objeto es un artículo que ha sido cuidadosamente diseñado para satisfacer tus necesidades. Es un producto de alta calidad que está hecho con materiales duraderos y resistentes, lo que significa que durará mucho tiempo. Este objeto también es muy versátil y se puede utilizar de muchas maneras diferentes, lo que lo convierte en una opción ideal para cualquier persona que busque un producto que pueda adaptarse a sus necesidades. Además, este objeto es muy fácil de usar y mantener, lo que lo hace perfecto para personas ocupadas o para aquellos que buscan un producto de bajo mantenimiento. En resumen, este objeto es una excelente opción para cualquiera que busque un producto de alta calidad y versátil que pueda satisfacer una amplia gama de necesidades.",
-    file: "javascript.png",
-    createdAt: "2023-03-31T12:17:17.736Z",
-    updatedAt: "2023-03-31T12:17:17.736Z",
-    UserId: 2
-  },
-  {
-    id: 5,
-    title: "Quinto Posteo",
-    description: "Este objeto es un artículo que ha sido cuidadosamente diseñado para satisfacer tus necesidades. Es un producto de alta calidad que está hecho con materiales duraderos y resistentes, lo que significa que durará mucho tiempo. Este objeto también es muy versátil y se puede utilizar de muchas maneras diferentes, lo que lo convierte en una opción ideal para cualquier persona que busque un producto que pueda adaptarse a sus necesidades. Además, este objeto es muy fácil de usar y mantener, lo que lo hace perfecto para personas ocupadas o para aquellos que buscan un producto de bajo mantenimiento. En resumen, este objeto es una excelente opción para cualquiera que busque un producto de alta calidad y versátil que pueda satisfacer una amplia gama de necesidades.",
-    file: "javascript.png",
-    createdAt: "2023-03-31T12:17:17.736Z",
-    updatedAt: "2023-03-31T12:17:17.736Z",
-    UserId: 1
-  }
-] */
-
-/* const user = {
-  id: 1,
-  username: "maria",
-  password: "clave123",
-  email: "maria@example.com",
-  birthdate: "1989-06-15",
-} */
-
-console.log(posts);
-
 const Posts = ()=>{
 
   const allPosts = useSelector(state => state.posts)
-  const user = useSelector(state => state.actualUser)
+  // const user = useSelector(state => state.actualUser)
 
   const dispatch = useDispatch();
 
