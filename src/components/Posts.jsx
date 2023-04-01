05fa2687d7442db81629283134d7d9d7dee38438
Lukkas98 <lucaspalma988@gmail.com>
2023-04-01 13:58:58 -0300

    fix style home

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -15,8 +15,8 @@ const Posts = ()=>{
   },[dispatch])
 
     return(
-      <div className="flex flex-wrap justify-center p-4">
-        <div className="overflow-hidden min-h-0 h-5/6">
+      <div className="flex flex-wrap justify-center p-4 w-full">
+        <div className="overflow-hidden min-h-0 h-5/6 w-full">
           <div className="flex flex-col gap-2 overflow-y-auto h-full scrollbar-thin scrollbar-thumb-gray-300 scrollbar-track-transparent">
             {allPosts?.map((post, i) => (
               <Post post={post} key={i} />
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -6,7 +6,7 @@ import { useSelector } from "react-redux";
 const Home = () => {
 
   return (
-    <div className="flex justify-start flex-col items-center h-full overflow-hidden">
+    <div className="flex justify-start flex-col items-center h-full overflow-hidden w-full">
       <NavLink to="/createPost" className="p-2 m-4 rounded-md bg-gray-400 transition-all duration-500 hover:bg-gray-300 hover:scale-150">Sube un posteo</NavLink>
       
       <Posts />
