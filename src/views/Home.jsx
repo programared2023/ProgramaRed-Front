8d742271e1fa4df4464d652ce3c5a1f0d38d4eef
juncos22 <juncosnicolas36@gmail.com>
2023-04-04 18:57:28 -0300

    Update Home.jsx

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,23 +1,42 @@
-import { NavLink, useLocation, useParams, useSearchParams } from "react-router-dom";
+import { NavLink, useSearchParams } from "react-router-dom";
 import Posts from "../components/Posts";
 import Filters from "../components/Filters/Filters";
 import { useDispatch } from "react-redux";
 import { getAllPosts } from "../redux/actions";
-import { useEffect, useState } from "react";
-import { initMercadoPago, Wallet } from "@mercadopago/sdk-react";
+import { useCallback, useEffect } from "react";
 import axios from "axios";
 
-const MP_PUBLIC_KEY = "TEST-9cd5ef81-313e-40ed-9aa7-f8d0f163a1e2"
+// const MP_PUBLIC_KEY = "TEST-5511f904-81cd-4d76-b9f5-72f286ec7543"
+const FORM_ID = "payment-form"
+
 const Home = () => {
   const dispatch = useDispatch();
-  initMercadoPago(MP_PUBLIC_KEY)
-  const [preferenceId, setPreferenceId] = useState("")
-  const [searchParams, setSearchParams] = useSearchParams()
+  const [searchParams, _] = useSearchParams()
 
-  console.log(searchParams);
   useEffect(() => {
     dispatch(getAllPosts())
-  }, [dispatch])
+  }, [dispatch, paymentData])
+
+  const realizarPago = useCallback(async () => {
+    const res = await axios.post('http://localhost:3001/subcriptions',
+      {
+        "title": "Subscripcion Premium",
+        "description": "Pagar una subscripcion premium para poder publicar videos",
+        "price": 500,
+        "user": {
+          "username": "nicolas123",
+          "email": "juncosnicolas36@gmail.com"
+        }
+      })
+    if (res.data.preferenceId) {
+      const script = document.createElement('script')
+      script.type = 'text/javascript'
+      script.src = 'https://www.mercadopago.com.ar/integrations/v1/web-payment-checkout.js'
+      script.setAttribute('data-preference-id', res.data.preferenceId)
+      const form = document.getElementById(FORM_ID)
+      form.appendChild(script)
+    }
+  })
 
   return (
     <div className="flex justify-start flex-col items-center h-full overflow-hidden w-full">
@@ -29,28 +48,8 @@ const Home = () => {
           <Posts />
         </div>
       </div>
-
-      <Wallet onReady={() => {
-        console.log('Ready:', 'Pago listo para crearse');
-      }}
-        onSubmit={async () => {
-          const res = await axios.post('http://localhost:3001/subcriptions',
-            {
-              "title": "Subscripcion Premium",
-              "description": "Pagar una subscripcion premium para poder publicar videos",
-              "price": 500,
-              "user": {
-                "username": "nicolas123",
-                "email": "juncosnicolas36@gmail.com"
-              }
-            })
-          if (res?.data.preferenceId) {
-            console.log(res?.data);
-            setPreferenceId(res.data.preferenceId)
-          }
-        }}
-        onError={(error) => { console.error('Error:', error); }}
-        initialization={{ preferenceId: preferenceId }} />
+      <form id={FORM_ID} method="get" />
+      <button onClick={realizarPago} className="bg-blue-500 px-3 py-1 hover:bg-blue-700 rounded-xl border border-black">Realizar Pago</button>
 
       {
         searchParams.get('error') && <span className="text-red-700">Hubo un error al realizar el pago</span>
@@ -74,3 +73,9 @@ export default Home;
 
 //navBar izquierda
 //links
+
+/**
+ * CREDENCIALES DE PRUEBA MERCADO PAGO
+ * USERNAME: TEST_USER_2109952451
+ * PASSWORD: dJqrzvx9YU 
+ */
