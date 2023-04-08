834c20d94e657204dd57c73f792fa0ec323335a0
Sergio Suárez <serrsua.88@gmail.com>
2023-04-08 20:37:13 -0300

    About

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -5,13 +5,13 @@ import camilo from "../images/Camilo.jpg";
 import pumba from "../images/Pumba.jpg";
 import niko from "../images/Niko.jpg";
 import sergio from "../images/Sergio.jpg";
-
+import thomas from "../images/Thomas.jpg";
 
 const About = () => {
   return (
-    <div className=" flex-col flex justify-around items-center bg-slate-500 h-full w-full lg:flex-row ">
-      <div>
-        <div className="flex flex-col items-center mb-4 md:mb-0">
+    <div className=" flex-col flex justify-between items-center bg-slate-500 h-full w-full lg:flex-row ">
+      <div className=" ml-14">
+        <div className=" flex flex-col items-center mb-4 md:mb-0">
           <img
             src={logo}
             alt="logo"
@@ -31,28 +31,51 @@ const About = () => {
         </div>
       </div>
       <div>
-        <div>
-          <img className=" rounded-full w-20 " src={ailing} alt="" />
-          <p>Ailin Galante Rosso</p>
-          <img className=" rounded-full w-20 " src={pumba} alt="" />
-          <p>Franco David Segovia</p>
-          <img className=" rounded-full w-20 " src={camilo} alt="" />
-          <p>Jeffer Camilo Romero Perez</p>
-          <img className=" rounded-full w-20 " src={lucas} alt="" />
-          <p>Lucas Palma</p>
-          <img className=" rounded-full w-20 " src={niko} alt="" />
-          <p>Nicolas Eduardo Juncos</p>
-          <img className=" rounded-full w-20 " src={sergio} alt="" />
-          <p>Sergio Rodrigo Suarez</p>
-          <img className=" rounded-full w-20 " src="" alt="" />
-          <p>Thomas Enrique Naquiche Leon</p>
+        <div className=" flex flex-col items-center font-semibold text-white shrink-0 ">
+          <div className="flex gap-2 ">
+            <div className=" w-44 flex flex-col items-center justify-center">
+              <img className=" rounded-full w-20 " src={ailing} alt="" />
+              <p className=" text-center">Ailin Galante Rosso</p>
+            </div>
+            <div className=" w-44 flex flex-col items-center justify-center">
+              <img className=" rounded-full w-20 " src={pumba} alt="" />
+              <p className=" text-center">Franco David Segovia</p>
+            </div>
+            <div className=" w-44 flex flex-col items-center justify-center">
+              <img className=" rounded-full w-20 " src={camilo} alt="" />
+              <p className=" text-center">Jeffer Camilo Romero Perez</p>
+            </div>
+          </div>
+          <div className="flex gap-2">
+            <div className=" w-44 flex flex-col items-center justify-center">
+              <img className=" rounded-full w-20 " src={lucas} alt="" />
+              <p className=" text-center">Lucas Palma</p>
+            </div>
+            <div className=" w-44 flex flex-col items-center justify-center">
+              <img className=" rounded-full w-20 " src={niko} alt="" />
+              <p className=" text-center">Nicolas Eduardo Juncos</p>
+            </div>
+            <div className=" w-44 flex flex-col items-center justify-center">
+              <img className=" rounded-full w-20 " src={sergio} alt="" />
+              <p className=" text-center">Sergio Rodrigo Suarez</p>
+            </div>
+            <div className=" w-44 flex flex-col items-center justify-center">
+              <img className=" rounded-full w-20 " src={thomas} alt="" />
+              <p className=" text-center">Thomas Enrique Naquiche Leon</p>
+            </div>
+          </div>
         </div>
       </div>
-      <div>
-        <h2 className="text-lg font-bold text-gray-800 mt-4">
+      <div className="mr-14">
+        <h2 className=" text-lg font-bold text-gray-800 mt-4">
           Desarrolladores
         </h2>
-        <p></p>
+        <p>
+            ProgramaRed es una red social creada para programadores y
+            desarrolladores. En nuestra plataforma, puedes encontrar otros
+            programadores con intereses similares, colaborar en proyectos,
+            compartir conocimientos y mucho más.
+          </p>
       </div>
     </div>
   );
