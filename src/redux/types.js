2dc382c226e4d00fd43a01c6d47ea09256820733
Lucas Palma <85300897+Lukkas98@users.noreply.github.com>
2023-04-09 20:55:11 -0300

    Update types.js

--- src/redux/types.js
+++ src/redux/types.js
@@ -10,4 +10,4 @@ export const GET_POST_BY_TAG = "GET_POST_BY_TAG";
 export const CLEAR_FILTERS = "CLEAR_FILTERS";
 export const GET_POST_BY_SEARCH = "GET_POST_BY_SEARCH";
 export const SET_CATEGORY = "SET_CATEGORY";
-export const GET_fAVORITES = "GET_FAVORITES";
+export const GET_FAVORITES = "GET_FAVORITES";
