07879a568c8ca3ec0004e875a074daf16f4b776b
Sergio Suárez <serrsua.88@gmail.com>
2023-04-11 13:59:19 -0300

    NotFound

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,13 +1,21 @@
 import { NavLink } from "react-router-dom";
-
+import notFound from "../images/NotFoundGif.gif"
 
 const NotFound = () => {
-    return (
-        <div>
-            <h1>Page not Found!!</h1>
-            <NavLink to="/" >Home</NavLink>
-        </div>
-    )
-}
+
+    const id = localStorage.getItem("id");
+
+  return (
+    <div className="flex flex-col justify-center items-center h-screen overflow-hidden">
+      <img src={notFound} alt="" />
+      {
+        !id ? 
+        <NavLink to="/" >Volver</NavLink>
+        :
+        <NavLink to="/home" >Volver</NavLink>
+      }
+    </div>
+  );
+};
 
 export default NotFound;
