8b796c8a202cce8d12350ffcf89fc306eb4951e5
Lukkas98 <lucaspalma988@gmail.com>
2023-04-12 17:24:45 -0300

    add alerts

--- src/components/Fav.jsx
+++ src/components/Fav.jsx
@@ -3,77 +3,83 @@ import { useState } from "react";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getFavorites } from "../redux/actions";
+import Swal from "sweetalert2";
 
-const Fav = ({postId, localUser}) => {
+const Fav = ({ postId, localUser }) => {
+  const [userId, setUserId] = useState("");
+  const dispatch = useDispatch();
 
-    const [userId, setUserId] = useState("")
-    const dispatch = useDispatch()
-
-    useEffect(() => {
-        let id = localStorage.getItem("id")
-        if(id){
-            setUserId(id)
-            dispatch(getFavorites(id))
-        }
-    }, [dispatch])
-    
-    console.log("userId:", userId)
-
-
-    const [clicked, setClicked] = useState(false);
-    const  allFavorites = useSelector(state => state.favorites)
-    
-    useEffect(() => {
-        let selectedFavorite = allFavorites.find(fav => Number(fav.PostId) === Number(postId));
-        if(selectedFavorite !== undefined){
-            setClicked(true)
-        }else{
-            setClicked(false)
-        }
-    }, [allFavorites])
-
-    const favorite = {
-        idPost: postId,
-        idUser: localUser,
-      };
-
-
-      console.log(clicked)
-
-    const favoriteHandler = async () => {
-        await axios.post(`/favorites`, favorite);
-        setClicked(true)
-
+  useEffect(() => {
+    let id = localStorage.getItem("id");
+    if (id) {
+      setUserId(id);
+      dispatch(getFavorites(id));
     }
+  }, [dispatch]);
 
-    const deleteFavorite = async () => {
-        await axios.delete(`/favorites`, { data: favorite });
-        setClicked(false)
+  console.log("userId:", userId);
+
+  const [clicked, setClicked] = useState(false);
+  const allFavorites = useSelector((state) => state.favorites);
+
+  useEffect(() => {
+    let selectedFavorite = allFavorites.find(
+      (fav) => Number(fav.PostId) === Number(postId)
+    );
+    if (selectedFavorite !== undefined) {
+      setClicked(true);
+    } else {
+      setClicked(false);
     }
+  }, [allFavorites]);
 
+  const favorite = {
+    idPost: postId,
+    idUser: localUser,
+  };
 
-        return(
-            <div>
-                <div onClick={clicked ? deleteFavorite : favoriteHandler}>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill={!clicked ? "none" : "currentColor"}
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  className="w-6 h-6"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z"
-                  />
-                </svg>
-              </div>
-            </div>
-        )
-    
-}
+  const favoriteHandler = async () => {
+    await axios.post(`/favorites`, favorite);
+    setClicked(true);
+    Swal.fire({
+      icon: "success",
+      title: "Favorito Agregado",
+      showConfirmButton: false,
+      timer: 1200,
+    });
+  };
 
-export default Fav
+  const deleteFavorite = async () => {
+    await axios.delete(`/favorites`, { data: favorite });
+    setClicked(false);
+    Swal.fire({
+      icon: "success",
+      title: "Favorito eliminado",
+      showConfirmButton: false,
+      timer: 1200,
+    });
+  };
 
+  return (
+    <div>
+      <div onClick={clicked ? deleteFavorite : favoriteHandler}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill={!clicked ? "none" : "currentColor"}
+          viewBox="0 0 24 24"
+          strokeWidth="1.5"
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z"
+          />
+        </svg>
+      </div>
+    </div>
+  );
+};
+
+export default Fav;
