df51f3bd4c6c72dc6463f05f3b923b787606f761
juncos22 <juncosnicolas36@gmail.com>
2023-04-13 13:08:44 -0300

    dashboard admin con diseño basico

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import FalseScreen from "./components/FalseScreen";
 import Detail from "./views/Detail";
 import About from "./views/About";
 import NotFound from "./components/NotFound";
+import DashboardAdmin from "./views/Admin";
 
 function App() {
   const { pathname } = useLocation();
@@ -21,8 +22,8 @@ function App() {
   const toggleDetails = () => setShowDetails(!showDetails);
 
   return (
-    <div className={`DIV_APP grid justify-center bg-veryLigthGreen lg:grid-cols-desktop_lg xl:grid-cols-desktop_xl ${pathname === "/" ? "lg:grid-cols-1 justify-items-center" : "" } ${pathname === "/home" ? "h-screen" : ""}${pathname === "/premium" ? "grid-rows-4 lg:grid-rows-1" : ""}${pathname === "/about" ? "grid-rows-1 lg:grid-cols-1" : ""}`}>
-     {/*  {pathname !== "/" && pathname !== "/signUp" && pathname !== "/about" && <NavBar />} */}
+    <div className={`DIV_APP grid justify-center bg-veryLigthGreen lg:grid-cols-desktop_lg xl:grid-cols-desktop_xl ${pathname === "/" ? "lg:grid-cols-1 justify-items-center" : ""} ${pathname === "/home" ? "h-screen" : ""}${pathname === "/premium" ? "grid-rows-4 lg:grid-rows-1" : ""}${pathname === "/about" ? "grid-rows-1 lg:grid-cols-1" : ""}`}>
+      {/*  {pathname !== "/" && pathname !== "/signUp" && pathname !== "/about" && <NavBar />} */}
       {(pathname.startsWith("/profile") || pathname === "/home" || pathname === "/createPost" || pathname === "/favorites" || pathname === "/singUp" || pathname === "/premium") && <NavBar />}
       {showDetails && <FalseScreen isView={showDetails} />}
       {showDetails && <Detail toggleDetails={toggleDetails} />}
@@ -30,12 +31,13 @@ function App() {
         <Route path="*" element={<NotFound />} />
         <Route path="/" element={<Landing />} />
         <Route path="/about" element={<About />} />
-        <Route path="/home" element={<Home toggleDetails={toggleDetails} />}/>
+        <Route path="/home" element={<Home toggleDetails={toggleDetails} />} />
         <Route path="/createPost" element={<CreatePost />} />
         <Route path="/profile/:id" element={<Profile toggleDetails={toggleDetails} />} />
         <Route path="/favorites" element={<Favorites />} />
         <Route path="/signUp" element={<SignUp />} />
         <Route path="/premium" element={<Payment />} />
+        <Route path="/admin" element={<DashboardAdmin />} />
       </Routes>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import * as ReactDOMClient from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
@@ -10,20 +10,22 @@ import { Auth0Provider } from "@auth0/auth0-react";
 // axios.defaults.baseURL = 'http://localhost:3001';
 axios.defaults.baseURL = "https://programared-back-production.up.railway.app";
 
+const container = document.getElementById('root');
+// Create a root.
+const root = ReactDOMClient.createRoot(container);
 
-ReactDOM.render(
+root.render(
   <Provider store={store}>
     <BrowserRouter>
       <Auth0Provider
-      domain="dev-ld1rfpxkhqa8gz6z.us.auth0.com"
-      clientId="WaIt30DwXfzg00vAvdwrytBgnJ9sWG4r"
-      redirectUri={window.location.origin}
-      audience="programared"
-      scope="openid profile email"
+        domain="dev-ld1rfpxkhqa8gz6z.us.auth0.com"
+        clientId="WaIt30DwXfzg00vAvdwrytBgnJ9sWG4r"
+        redirectUri={window.location.origin}
+        audience="programared"
+        scope="openid profile email"
       >
         <App />
-      </Auth0Provider>  
+      </Auth0Provider>
     </BrowserRouter>
-  </Provider>,
-  document.getElementById('root')
+  </Provider>
 );
